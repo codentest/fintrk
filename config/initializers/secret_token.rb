3adc62a823c9db322297b352740f6e11da8500f8
root <root@secondsoul.(none)>
2012-08-18 07:29:48 -0500

    Demo app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Finone::Application.config.secret_token = '5fed6b52ab532e5f7d8a5a570cbf9491b63ca3adcfed5af4100359590eba8d33bf9775a18b2a844a8052b6345ab5b8bc661625cd4a84d6a3d7a82bb12954cb3c'
